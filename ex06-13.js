456b6c0b56ae9513e9b3290bc462681e84ae0b1a
taka14110165 <taka14110165@gmail.com>
2018-12-02 20:55:15 -0500

    message

---
+++ ex06-13.js
@@ -0,0 +1,13 @@
+
+
+
+function ThreeSecChange3(){
+  //関数imgchange12()を3000ミリ秒間隔で呼び出す 
+Timer("imgchange3()",3000); }
+
+function  imgchange3(){
+  document.getElementById(`logo2`).src = `./image/bulbon.gif`;
+  
+}
+
+
