f6dc689702d90ca65fa58138a1eab789e9aeddf1
taka14110165 <taka14110165@gmail.com>
2019-01-29 01:52:00 -0500

    message

---
+++ ex08-1.js
@@ -0,0 +1,8 @@
+function bloodtype(){
+  if(document.getElementById(`a`).value !='no'){alert(`AB`);}
+  else if(document.getElementById(`b`).value !='yes'){alert(`O`);}    
+  else{alert('B')}
+}
+
+
+
